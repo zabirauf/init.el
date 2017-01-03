52cdb7c22bc3e987f1b08c8e01e178b88a7ad00b
Zohaib Rauf <zabirauf@gmail.com>
2017-01-03 23:15:18 +0500

    Let there be light

---
+++ init.el
@@ -0,0 +1,32 @@
+;; Setting the package repositories
+(setq package-archives
+    '(("gnu" . "http://elpa.gnu.org/packages/")
+     ("marmalade" . "http://marmalade-repo.org/packages/")
+     ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;; Setting apropos to sort by score
+(setq apropos-sort-by-scores t)
+
+;; Enabling the windmove by default which allows to switch
+;; to windows using SHIFT <left>, <right>, <up>, <down>
+(windmove-default-keybindings)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes
+   (quote
+    ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(winner-mode t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
